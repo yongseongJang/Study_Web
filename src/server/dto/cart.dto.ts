1f3e122c743527f6d3df104bb67e57cf1e9ea945
yongseongJang <kgucs13@gmail.com>
2022-02-12 11:27:46 +0900

    fix(server): seperate cart entity and dto

---
+++ src/server/dto/cart.dto.ts
@@ -0,0 +1,58 @@
+import { Product as IProduct, Cart as ICart } from "../interfaces";
+import { Cart } from "../models/entity";
+
+export class AddToCartDto {
+  userId: number;
+  productId: number;
+  option: string;
+  quantity: number;
+
+  constructor(cart: ICart) {
+    this.userId = cart.userId!;
+    this.productId = cart.productId;
+    this.option = cart.option;
+    this.quantity = cart.quantity;
+  }
+
+  public toEntity() {
+    return Cart.from(this.userId, this.productId, this.option, this.quantity);
+  }
+}
+
+export class ReadCartDto {
+  productId: number;
+  option: string;
+  quantity: number;
+  category: string;
+  productInfo: Pick<IProduct, "name" | "price" | "salePrice" | "image">;
+
+  constructor(cart: ICart) {
+    this.productId = cart.productId;
+    this.option = cart.option;
+    this.quantity = cart.quantity;
+    this.category = cart.category!;
+    this.productInfo = cart.productInfo!;
+  }
+
+  static from(
+    productId: number,
+    option: string,
+    quantity: number,
+    product: IProduct,
+  ) {
+    const readCartDto = new ReadCartDto({
+      productId,
+      option,
+      quantity,
+      category: product.productCategory![0].category?.name,
+      productInfo: {
+        name: product.name,
+        price: product.price,
+        salePrice: product.salePrice,
+        image: product.image,
+      },
+    });
+
+    return readCartDto;
+  }
+}
--- src/server/utils/validation/schemas/cartSchema.ts
+++ src/server/utils/validation/schemas/cartSchema.ts
@@ -2,8 +2,8 @@ import * as Joi from "@hapi/joi";
 
 export const cartSchema = Joi.object({
   _id: Joi.number(),
-  user_id: Joi.number().required(),
-  product_id: Joi.number().required(),
+  userId: Joi.number().required(),
+  productId: Joi.number().required(),
   quantity: Joi.number().required(),
   option: Joi.string().required(),
 });
